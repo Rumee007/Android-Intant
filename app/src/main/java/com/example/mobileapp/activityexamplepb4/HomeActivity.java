5a41d98ecd45c1d21c7438b24ad7909b3621edb1
rumee007 <37110449+Rumee007@users.noreply.github.com>
2018-03-07 02:43:17 +0600

    Show username and password in new intent text field

--- app/src/main/java/com/example/mobileapp/activityexamplepb4/HomeActivity.java
+++ app/src/main/java/com/example/mobileapp/activityexamplepb4/HomeActivity.java
@@ -14,22 +14,18 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
-        //
         // Call intent
         Intent incomingIntent = getIntent();
         // Get value
         String userEmail = incomingIntent.getStringExtra("email");
         String userPassword = incomingIntent.getStringExtra("password");
-        // Get Value
+        // Set Value
         greetingsValue = findViewById(R.id.greetingsTextView);
         showEmailValue = findViewById(R.id.showEmailTextView);
         showPasswordValue = findViewById(R.id.showPasswordTextView);
-        //Set Value
+
         greetingsValue.setText("Welcome to Intent!");
         showEmailValue.setText(userEmail);
         showPasswordValue.setText(userPassword);
-
-        //Toast.makeText(this, userEmail + ", " + userPassword, Toast.LENGTH_LONG).show();
     }
 }
